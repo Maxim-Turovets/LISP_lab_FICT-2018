db46218e09863f8fb2860f3013ce2e02e35fd736
Maxim-Turovets <36541363+Maxim-Turovets@users.noreply.github.com>
2018-10-15 14:22:39 +0300

    add Task 1

--- Lab3_MaxT/lab3.lisp
+++ Lab3_MaxT/lab3.lisp
@@ -1,1 +1,22 @@
 :Task 1
+; lambda
+(defun fact_lambda (n)
+  ((lambda (n)
+    (cond
+      ((eq n 1) 1)
+      (t (* n (fact_lambda (- n 1))))))
+  n)
+)
+
+; let
+(defun fact_let (n)
+  (let ((n
+      (cond
+        ((eq n 1) 1)
+        (t (* n (fact_let (- n 1))))
+      )
+    ))
+    n)
+)
+(print(fact_lambda 3))
+(print(fact_let 3))
