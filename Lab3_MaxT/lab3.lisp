8abc60bd62b254c76f4777997141765a83ea2b06
Maxim-Turovets <36541363+Maxim-Turovets@users.noreply.github.com>
2018-10-15 14:56:48 +0300

    add task 6

--- Lab3_MaxT/lab3.lisp
+++ Lab3_MaxT/lab3.lisp
@@ -20,3 +20,22 @@
 )
 (print(fact_lambda 3))
 (print(fact_let 3))
+
+:Task 6
+(defun sw (s)
+  (map 'list #'string s))
+ 
+(defun az (w a b)
+  (cond ((null w) nil)
+        ((and (string= (car w) a) (string= (caddr w) b))
+         (cons (car w) (az (cddr w) a b)))
+        ((cons (car w) (az (cdr w) a b)))))
+ 
+(defun cn (w)
+  (cond ((null w) nil)
+        ((concatenate 'string (car w) (cn (cdr w))))))
+ 
+(defun a-delete-b (s a b)
+  (cn (az (sw s) a b)))
+ 
+(print (a-delete-b "abcq2w abq9wc" "q" "w"))
