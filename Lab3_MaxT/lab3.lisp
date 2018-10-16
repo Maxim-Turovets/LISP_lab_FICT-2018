55c3a85a8221460497a08e132720afbfb717fd08
Maxim-Turovets <36541363+Maxim-Turovets@users.noreply.github.com>
2018-10-16 15:51:14 +0300

    Update lab3.lisp

--- Lab3_MaxT/lab3.lisp
+++ Lab3_MaxT/lab3.lisp
@@ -92,9 +92,7 @@
         ((> count 0) (cons (car list1) (ins (cdr list1) list2 (- count 1))))
         ((append list2  list1))))
     l1 l2 ct )   
-)
-        
-     
+) 
                      ;list1     list2   count
        (print( ins '(1 2 3 4) '(5 6 7 8) 4))
 
