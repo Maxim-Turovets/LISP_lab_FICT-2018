d3acedfb17a4f4e3a9508d3aacaa9673a791bfa2
Maxim-Turovets <36541363+Maxim-Turovets@users.noreply.github.com>
2018-11-08 14:35:07 +0200

    Update lab4_MaxT/lab4.lisp

--- lab4_MaxT/lab4.lisp
+++ lab4_MaxT/lab4.lisp
@@ -1,18 +1,37 @@
 
-    
-    
+(setq aq (make-array 10 :fill-pointer 0))
+
 ;http://lisper.ru/pcl/practical-a-simple-database
 
 ;Выставки и конференции
 
 (defun make-ts (title type style)
-(list :title title :type type :style style))
+(list :title title :type type :style style)
+)
 
+ (defun vs-sort (v n)
+   (let ((vi 0) (vj 0))  
+     (dotimes (i n v)
+       (dotimes (j n t) 
+         (when (> j i)
+           (setf vi (aref v i))
+           (setf vj (aref v j))
+           (when (string> vi vj) (setf (aref v i) vj) (setf (aref v j) vi)))))))
+
+(defun getByNumF (lst n)   ;; n-й элемент с начала
+   (do ((i 1 (+ i 1)))
+       ((= i n) (car lst))
+       (setq lst (cdr lst))))
+       
 ;Глобальная переменная
 (defvar *db* nil)
 
 ; Добавления записи в базу данных
-(defun add-record (ts) (push ts *db*))
+(defun add-record (ts)
+   (push ts *db*)
+       (vector-push (getByNumF ts 2) aq)
+
+)
 
 ; Выводит содержимое базы данных в более читабельный форме
 (defun dump-db ()
@@ -27,6 +46,15 @@
 (defun type-selector (type)
 (lambda (ts) (equal (getf ts :type) type)))
 
+
+; сортировка 
+ (defun sort(n)
+    (vs-sort aq (- n 1))
+    (loop for i from 0 to n do 
+    (print (select(where :title (elt aq i)) ))
+    )
+ )
+ 
 ; Генерирует выражение выбора, которое возвращает все записи о выставках, которые совпадают со значениями, заданными в where
 (defun where (&key title type style)
 (lambda (ts)
@@ -54,23 +82,43 @@ row) *db*)))
 (defun make-comparison-expr (field value)
 (list 'equal (list 'getf 'ts field) value))
 
+
+          
+
 ; добавление значений в базу
- (print "Add info")
+;  (print "Add info")
  (add-record (make-ts "Flowers" "Сreative" "Modern"))
- (add-record (make-ts "Pictures" "Сreative" "PostModern"))
+  (add-record (make-ts "Bfghjuytfg" "Scientific" "Nowadays"))
+  (add-record (make-ts "Pict==ures" "Сrea" "PostModern"))
  (add-record (make-ts "Electronics" "Scientific" "Nowadays"))
- (dump-db)
+ (add-record (make-ts "Aguygb" "Сrea" "PostModern"))
+  (add-record (make-ts "Pictures" "Сreative" "PostModern"))
+
+  (dump-db)
  
- ;  поиск
- (print "Search 1")
- (print(select(type-selector "Scientific")))
- (print "Search 2")
- (print(select(where :title "Flowers")))
+;    поиск
+;   (print "Search 1")
+;   (print(select(type-selector "Scientific")))
+;   (print "Search 2")
+
+;   (print(select(where :title "Flowers")))
  
- ;обновление данных
- (print "Update")
- (print(update(where :style "PostModern") :style "Avangard"))
+; ;обновление данных
+;   (print "Update")
+;   (print(update(where :style "PostModern") :style "Avangard"))
  
+;   (print "Sort")
+;   (dump-db)
+
   ; удаление 
- (print "Delete")
- (print (delete-rows(where :title "Pictures" )))
+;  (print "Delete")
+;  (print (delete-rows(where :title "Pictures" )))
+
+
+ ; сортировка
+ (sort 6)
+
+
+
+
+
